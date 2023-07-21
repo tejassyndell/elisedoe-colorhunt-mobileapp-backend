4f8f56d3cf00f17c93ee548f01d6961b18e57d09
sinamartin <developersweb001@gmail.com>
2023-07-21 11:05:42 +0530

    update api Wishlist api add

--- server/router/router.js
+++ server/router/router.js
@@ -2,18 +2,24 @@
 const express = require('express')
 const Routes = express.Router()
 const {
-  getProductName,
-  getCategories
+  grlAllArticles,
+  getCategories,
+  grlAllArticlesscroll,
+  getWishlist,
+  AddWishlist,
 } = require('../controller/controllerM2.js')
 
 
 //for login auth
 
-
+// grlAllArticles
 // //for dashboard
 // //---------------------new change 28-----------------------
-Routes.get('/getProductName',getProductName);
-Routes.get('/getCategories',getCategories);
+Routes.get('/grlAllArticles', grlAllArticles);
+Routes.get('/getCategories', getCategories);
+Routes.get('/grlAllArticlesscroll', grlAllArticlesscroll);
+Routes.post('/getWishlist', getWishlist);
+Routes.post('/AddWishlist', AddWishlist);
 
 // //---------------------new change 28-----------------------
 
